042a6beed263f251aafa4ff50f7571d10c119ed1
David Ricardo <r.arteta@utp.edu.co>
2023-08-10 15:26:28 -0500

    p-menubar with css working

--- WRivasWFront/src/app/app.module.ts
+++ WRivasWFront/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { StyleClassModule } from 'primeng/styleclass';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MenubarModule,
+    StyleClassModule
   ],
   providers: [],
   bootstrap: [AppComponent]
